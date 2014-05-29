9741c3d4c732267c3429a3ac83cb5099d015f50b
aq2bq <pememo@gmail.com>
2014-05-29 15:41:54 +0900

    Add an example

--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,5 +1,9 @@
+# encoding: utf-8
+
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
-  specify { expect(NameIndex.create_index([])).to eq [] }
+  let(:names) { ['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'] }
+  let(:expected) { [ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']] ] }
+  specify { expect(NameIndex.create_index(names)).to eq expected }
 end
