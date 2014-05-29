f3e25356f8f913a6b0dfdf9e642bc394e2990bce
aq2bq <pememo@gmail.com>
2014-05-29 16:39:40 +0900

    Implement

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,9 @@
+# encoding: utf-8
+
 class NameIndex
   def self.create_index(names)
-    []
+    %w(ア カ サ タ ナ ハ マ ヤ ラ ワ).zip(%w(オ ゴ ゾ ド ノ ポ モ ヨ ロ ン))
+    .map{|s| [s.first, names.select{|name| (s.first.ord..s.last.ord).include? name.ord}.sort]}
+    .delete_if{|result| result.last.empty?}
   end
 end
