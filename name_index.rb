8a3bed17f9851adea44a71f079ae2b31da9f7fc4
aq2bq <pememo@gmail.com>
2014-05-31 16:19:42 +0900

    Refactor: Dojo Effect!

--- name_index.rb
+++ name_index.rb
@@ -3,7 +3,9 @@
 class NameIndex
   def self.create_index(names)
     %w(ア カ サ タ ナ ハ マ ヤ ラ ワ).zip(%w(オ ゴ ゾ ド ノ ポ モ ヨ ロ ン))
-    .map{|s| [s.first, names.select{|name| (s.first.ord..s.last.ord).include? name.ord}.sort]}
-    .delete_if{|result| result.last.empty?}
+    .map{|first, last| 
+      results = names.select{|name| (first.ord..last.ord).include? name.ord}.sort
+      [first, results] unless results.empty?
+    }.compact
   end
 end
